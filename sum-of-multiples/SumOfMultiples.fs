b6e77d49a4947555238763124e1f5502aa9b27b6
Andrew Romanov <me@andrew-r.ru>
2017-09-10 22:51:53 +0300

    Add a solution for sum-of-multiples exercise

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,10 @@
+﻿module SumOfMultiples
+
+let isMultipleOf (numbers: int list) (target: int): bool =
+    numbers
+    |> List.exists (fun number -> target % number = 0)
+
+let sumOfMultiples (numbers: int list) (upperBound: int): int =
+    [1..(upperBound - 1)]
+    |> List.filter (isMultipleOf numbers)
+    |> List.sum
