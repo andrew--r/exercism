e16ea02e8e56b33130eeb36b616b51eeebc9ad78
Andrew Romanov <me@andrew-r.ru>
2017-09-17 19:03:46 +0300

    Add solution for the SpaceAge exercise

---
+++ space-age/SpaceAge.fs
@@ -0,0 +1,25 @@
+﻿module SpaceAge
+
+type Planet =
+    | Earth
+    | Mercury
+    | Venus
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+let getOrbitalPeriod (planet: Planet): decimal =
+    let earthYear = 31557600m
+    match planet with
+    | Earth -> earthYear
+    | Mercury -> earthYear * 0.2408467m
+    | Venus -> earthYear * 0.61519726m
+    | Mars -> earthYear * 1.8808158m
+    | Jupiter -> earthYear * 11.862615m
+    | Saturn -> earthYear * 29.447498m
+    | Uranus -> earthYear * 84.016846m
+    | Neptune -> earthYear * 164.79132m
+
+let spaceAge (planet: Planet) (seconds: decimal): decimal = System.Decimal.Round(seconds / getOrbitalPeriod planet, 2)
