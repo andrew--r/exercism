2b784810c524245727eed902663e551f6b4c6b49
Andrew Romanov <me@andrew-r.ru>
2017-10-15 02:11:28 +0300

    Refactor accumulate solution

--- accumulate/Accumulate.fs
+++ accumulate/Accumulate.fs
@@ -3,6 +3,6 @@
 let rec accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list =
     let rec transform list acc =
         match list with
-        | head :: tail -> transform tail (List.append acc [func head])
-        | [] -> acc
+        | head :: tail -> transform tail (func head::acc)
+        | [] -> List.rev acc
     transform input []
