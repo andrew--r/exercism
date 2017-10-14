8c17b3a14ef1c475658a4164dfe2f76b4893c512
Andrew Romanov <me@andrew-r.ru>
2017-10-15 01:27:28 +0300

    Add a solution for raindrops exercise

---
+++ raindrops/Raindrops.fs
@@ -0,0 +1,16 @@
+﻿module Raindrops
+
+let factors = [(3, "Pling"); (5, "Plang"); (7, "Plong")];
+
+let foldMessages acc (factor,message) = sprintf "%s%s" acc message
+
+let messageOrNumber number message =
+    match message with
+    | "" -> string number
+    | _ -> message
+
+let convert (number: int): string =
+    factors
+    |> List.filter (fun (factor, message) -> number % factor = 0)
+    |> List.fold foldMessages ""
+    |> messageOrNumber number
---
+++ raindrops/RaindropsTest.fs
@@ -0,0 +1,41 @@
+module RaindropsTest
+
+open Xunit
+open FsUnit.Xunit
+open Raindrops
+
+[<Theory>]
+[<InlineData(1, "1")>]
+[<InlineData(52, "52")>]
+[<InlineData(12121, "12121")>]
+let ``Non primes pass through`` number expected =
+    convert number |> should equal expected
+
+[<Theory>]
+[<InlineData(3)>]
+[<InlineData(6)>]
+[<InlineData(9)>]
+let ``Numbers containing 3 as a prime factor give pling`` (number) =
+    convert number |> should equal "Pling"
+
+[<Theory>]
+[<InlineData(5)>]
+[<InlineData(10)>]
+[<InlineData(25)>]
+let ``Numbers containing 5 as a prime factor give plang`` (number) =
+    convert number |> should equal "Plang"
+
+[<Theory>]
+[<InlineData(7)>]
+[<InlineData(14)>]
+[<InlineData(49)>]
+let ``Numbers containing 7 as a prime factor give plong`` (number) =
+    convert number |> should equal "Plong"
+
+[<Theory>]
+[<InlineData(15, "PlingPlang")>]
+[<InlineData(21, "PlingPlong")>]
+[<InlineData(35, "PlangPlong")>]
+[<InlineData(105, "PlingPlangPlong")>]
+let ``Numbers containing multiple prime factors give all results concatenated`` number expected =
+    convert number |> should equal expected
